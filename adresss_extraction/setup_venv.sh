5e21b120c9bbb96e7590dcae7b565b5e4893db3f
tp12121212 <tp12121212@users.noreply.github.com>
2025-11-30 17:11:31 +1100

    Set up virtual environment and install dependencies
    Added commands to install pip and PyPDF2 dependencies.

--- adresss_extraction/setup_venv.sh
+++ adresss_extraction/setup_venv.sh
@@ -1,3 +1,7 @@
 # Create and activate virtual environment
 python3.11 -m venv adresss_extraction
 source adresss_extraction/bin/activate
+
+# Install dependencies
+pip install --upgrade pip
+pip install PyPDF2
